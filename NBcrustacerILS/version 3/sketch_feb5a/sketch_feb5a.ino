cbc52300a0da7770b3fca741465f5f044b87d304
erwan62ezeque <erwan62.ezeque@gmail.com>
2026-04-07 16:52:33 +0200

    Add files via upload

---
+++ NBcrustacerILS/version 3/sketch_feb5a/sketch_feb5a.ino
@@ -0,0 +1,19 @@
+#include "nbcrustacier.h"
+
+const int pinILS=2; // 5v
+CCrustacier crust;// objet
+
+void setup() {
+Serial.begin(9600);
+pinMode(pinILS, INPUT_PULLUP); // input_pullup invers la logic
+}
+
+void loop() {
+bool etatILS =digitalRead(pinILS); // lit
+int nombre=crust.getVal(etatILS);
+Serial.print("passage detecter : ");
+Serial.print(nombre);
+Serial.print("");
+
+
+}
